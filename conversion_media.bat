2a819b233435f045ad222d96c2856b7937907ac7
Valentin DUMONT <valentin.dumont@etu.univ-nantes.fr>
2020-02-09 21:01:18 +0100

    Ajout porte 1900

--- conversion_media.bat
+++ conversion_media.bat
@@ -4,6 +4,8 @@
 
 ffmpeg -i gifporteactuel.mov -c:v libvpx-vp9 -b:v 2M -an -vf scale=1920:-1 doornow.webm
 ffmpeg -i gifporte70s.mov -c:v libvpx-vp9 -b:v 2M -an -vf scale=1920:-1 door70s.webm
+ffmpeg -i gifporte1900.mov -c:v libvpx-vp9 -b:v 2M -an -vf scale=1920:-1 door1900.webm
 
 ffmpeg -i gifporteactuel.mov -c:v libvpx-vp9 -b:v 2M -an -vf scale=1920:-1,reverse doornow_rev.webm
 ffmpeg -i gifporte70s.mov -c:v libvpx-vp9 -b:v 2M -an -vf scale=1920:-1,reverse door70s_rev.webm
+ffmpeg -i gifporte1900.mov -c:v libvpx-vp9 -b:v 2M -an -vf scale=1920:-1,reverse door1900_rev.webm
