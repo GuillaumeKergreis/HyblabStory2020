7b5ded6052d17e7c7b3bae5bc8e3cf78895491e8
Jeremy ANGORA <jeremy.angora@etu.univ-nantes.fr>
2020-02-10 23:45:56 +0100

    Ajoout des modifications du texte

--- sciencescom/public/data/Personnage.js
+++ sciencescom/public/data/Personnage.js
@@ -9,46 +9,46 @@ let Personnage = {
     "dialogues": {
       "0": [
         {
-          "texte": ["^1000Bonjour !^300 je suis Emma.^300 Je suis en Master 1 à Audencia,^150 je peux t’aider?^500"],
+          "texte": ["Bonjour et bienvenu aux portes ouvertes d'Audencia ! je suis Emma et je suis en Master 1 à Audencia, je peux t’aider ?"],
           "suivant": {"personnage": "Pablo", "slide": "0", "paragraphe": "0"}
         },
         {
-          "texte": ["Bien sûr,^150 je suis la pour ça et mes camarades aussi !^300", "Alors si tu me permets,^150 je vais te faire voyager dans le temps à travers l’histoire d’Audencia,^300 suis-moi !^500"]
+          "texte": ["Bien sûr, je suis là pour ça et mes camarades aussi ! Alors si tu me permets, je vais te faire voyager dans le temps à travers l’histoire d’Audencia, suis-moi !"]
         }
       ],
       "1": [
         {
-          "texte": ["^1500Super,^150 on vient d’arriver,^150 pile à l’endroit que je voulais !^500"],
+          "texte": ["Super, on vient d’arriver, pile à l’endroit où je voulais !"],
           "suivant": {"personnage": "Pablo", "slide": "1", "paragraphe": "0"}
         },
         {
-          "texte": ["Toujours Nantes.^150 Nous n’avons pas changé de lieu,^150 mais de période!^500"],
+          "texte": ["Toujours Nantes. Nous n’avons pas changé de lieu, mais de période!"],
           "suivant": {"personnage": "Pablo", "slide": "1", "paragraphe": "1"}
         },
         {
-          "texte": ["Mais non,^150 ce sera très instructif tu vas voir.^150 On va commencer au commencement,^300", "lors des premières décennies de l’école,^150 au début du XXème siècle.^300", "Je te laisse choisir quel témoignage tu souhaite découvrir…"]
+          "texte": ["Mais non, ce sera très instructif tu vas voir. On va commencer par le commencement, lors des premières décennies de l’école, au début du XXème siècle. Je te laisse choisir quel témoignage tu souhaite découvrir…"]
         }
       ],
       "4": [
         {
-          "texte": ["Alors Pablo, j’espère que tu as pu découvrir un peu plus ce qu’était Audencia à ses débuts.^500", " C’est une grande histoire avec un passé comme tu as pu le remarquer.^500"," Mais ce n’est pas fini je vais t’emmener rencontrer d’autres personnes. Elles pourront t’en dire davantage. Continuons.^500"],
+          "texte": ["Alors Pablo, j’espère que tu as pu découvrir un peu plus ce qu’était Audencia à ses débuts. C’est une grande école avec une longue histoire comme tu as pu le remarquer. Mais ce n’est pas fini !Je vais t’emmener rencontrer d’autres personnes. Elles pourront t’en dire davantage. Continuons."],
           "suivant": {"personnage": "Pablo", "slide": "4", "paragraphe": "0"}
         },
         {
-          "texte": ["Bon, on a fait un petit bon dans le temps, et on est en…. 1974 !^500"],
+          "texte": ["Bon, on a fait un petit bon dans le temps, et on est en…. 1974 !"],
           "suivant": {"personnage": "Pablo", "slide": "4", "paragraphe": "1"}
         },
         {
-          "texte": ["Pas mal !  Et tu vas voir, l’école a aussi fait son bon en avant, et pas des moindres !^500"," Mais je suis convaincue que d’autres pourront mieux te l’expliquer que moi…^500"]
+          "texte": ["Pas mal !  Et tu vas voir, l’école a aussi fait son bond en avant, et pas des moindres ! Mais je suis convaincue que d’autres pourront mieux te l’expliquer que moi…"]
         }
       ],
       "7": [
         {
-          "texte": ["tu en sais un peu plus maintenant. J’ai encore une période à te faire découvrir. Tu verra tu ne sera pas déçu. C’est peut-être celle qui te parlera le plus."],
+          "texte": ["tu en sais un peu plus maintenant. J’ai encore une période à te faire découvrir. Tu verras tu ne seras pas déçu. C’est peut-être celle qui te parlera le plus."],
           "suivant": {"personnage": "Pablo", "slide": "7", "paragraphe": "0"}
         },
         {
-          "texte": ["À présent, nous sommes déjà sur la période contemporaine de l’école. Beaucoup des éléments qui s’y déroulent sont encore là dans l’école d’ailleurs !"],
+          "texte": ["À présent, nous sommes déjà dans la période contemporaine de l’école. Beaucoup des éléments qu'on retrouve ici existe toujours d’ailleurs !"],
           "suivant": {"personnage": "Pablo", "slide": "4", "paragraphe": "1"}
         },
         {
@@ -61,7 +61,7 @@ let Personnage = {
           "suivant": {"personnage": "Pablo", "slide": "10", "paragraphe": "0"}
         },
         {
-          "texte": ["Ca marche !"],
+          "texte": ["Ça marche !"],
           "suivant": {"personnage": "Emma", "slide": "10", "paragraphe": "2"}
         },
         {
@@ -87,41 +87,41 @@ let Personnage = {
     "dialogues": {
       "0": [
         {
-          "texte": ["Bonjour,^150 moi c’est Pablo,^150 Je suis à la recherche d’une école pour l’année prochaine,^300", "et je voulais avoir quelques renseignements au sujets de celle-ci.^500"],
+          "texte": ["Bonjour, moi c’est Pablo, Je suis à la recherche d’une école pour l’année prochaine, et je voulais avoir quelques renseignements au sujet de celle-ci."],
           "suivant": {"personnage": "Emma", "slide": "0", "paragraphe": "1"}
         }
       ],
       "1": [
         {
-          "texte": ["Et cet endroit, c’est…^500"],
+          "texte": ["Et cet endroit, c’est…"],
           "suivant": {"personnage": "Emma", "slide": "1", "paragraphe": "1"}
         },
         {
-          "texte": ["Ok, donc on est dans Retour vers le futur là ?^150 C’est un peu vieu jeu non ?^500"],
+          "texte": ["Ok, donc on est dans Retour vers le futur là ? C’est un peu vieux jeu non ?"],
           "suivant": {"personnage": "Emma", "slide": "1", "paragraphe": "2"}
         }
       ],
       "4": [
         {
-          "texte": ["J’ai hâte !^300"],
+          "texte": ["J’ai hâte !"],
           "suivant": {"personnage": "Emma", "slide": "4", "paragraphe": "1"}
         },
         {
-          "texte": ["Plutôt grand comme bon ! Fin des Trentes Glorieuses, VGE président, les premières télés en couleur…  je vois le dessin !^800"],
+          "texte": ["Plutôt grand comme bond ! Fin des Trentes Glorieuses, VGE président, les premières télés en couleur…  je vois le dessin !"],
           "suivant": {"personnage": "Emma", "slide": "4", "paragraphe": "2"}
         }
       ],
       "5": [
         {
-          "texte": ["Bonjour, je m’appelle Pablo, enchanté, je suis intéressé par l’école que tu as fait et je voulais savoir :^500"," comment tes années à l’école se sont passées ? Pas trop compliqué ?^500"],
+          "texte": ["Bonjour, je m’appelle Pablo, enchanté, je suis intéressé par l’école que tu as fait et je voulais savoir : comment tes années à l’école se sont passées ? Pas trop compliqué ?"],
           "suivant": {"personnage": "Alain", "slide": "5", "paragraphe": "0"}
         },
         {
-          "texte": ["D’accord, c’était vraiment le bon moment, alors !^500"],
+          "texte": ["D’accord, c’était vraiment le bon moment, alors !"],
           "suivant": {"personnage": "Alain", "slide": "5", "paragraphe": "1"}
         },
         {
-          "texte": ["J’imagine ! Et tous ces équipements t’ont aidés pendant tes années d’étudiant ?^500"],
+          "texte": ["J’imagine ! Et tous ces équipements t’ont aidé pendant tes années d’étudiant ?"],
           "suivant": {"personnage": "Alain", "slide": "5", "paragraphe": "2"}
         },
         {
@@ -129,25 +129,25 @@ let Personnage = {
           "suivant": {"personnage": "Alain", "slide": "5", "paragraphe": "3"}
         },
         {
-          "texte": ["Plutôt oui ! En tout cas, merci beaucoup d’avoir pris le temps de discuter avec moi, j’ai beaucoup appris !^500"],
+          "texte": ["Plutôt oui ! En tout cas, merci beaucoup d’avoir pris le temps de discuter avec moi, j’ai beaucoup appris !"],
           "suivant": {"personnage": "Alain", "slide": "5", "paragraphe": "4"}
         }
       ],
       "6": [
         {
-          "texte": ["Bonjour, je m’appelle Pablo, je suis très intéressé par l’école.^500"," J’ai pu en apprendre un peu plus sur ses débuts et je ne pensais pas qu’elle pouvait avoir une telle histoire.^500"," J’aimerais m’informer un peu plus.^400"],
+          "texte": ["Bonjour, je m’appelle Pablo, je suis très intéressé par l’école. J’ai pu en apprendre un peu plus sur ses débuts et je ne pensais pas qu’elle pouvait avoir une telle histoire. J’aimerais m’informer un peu plus."],
           "suivant": {"personnage": "Martine", "slide": "6", "paragraphe": "0"}
         },
         {
-          "texte": ["Pouvez-vous m'expliquer ce qu’est un MBA, c’est assez flou pour moi.^500"],
+          "texte": ["Pouvez-vous m'expliquer ce qu’est un MBA, c’est assez flou pour moi."],
           "suivant": {"personnage": "Martine", "slide": "6", "paragraphe": "1"}
         },
         {
-          "texte": ["je comprends, cela à dû être très intéressant à vivre.^500"],
+          "texte": ["je comprends, cela à dû être très intéressant à vivre."],
           "suivant": {"personnage": "Martine", "slide": "6", "paragraphe": "2"}
         },
         {
-          "texte": ["Je vous remercie de m’avoir consacré un peu de temps.^500"," Encore une fois l’histoire m’épate et me donne envie d’étudier dans cet établissement.^500"],
+          "texte": ["Je vous remercie de m’avoir consacré un peu de temps. Encore une fois l’histoire m’épate et me donne envie d’étudier dans cet établissement."],
           "suivant": {"personnage": "Martine", "slide": "6", "paragraphe": "3"}
         }
       ],
@@ -167,7 +167,7 @@ let Personnage = {
           "suivant": {"personnage": "Emma", "slide": "10", "paragraphe": "1"}
         },
         {
-          "texte": ["Oui ! Je dois dire, je suis plutôt impressionné ! Je ne pensais pas qu’il y aurait autant d’histoire pour cette école !"],
+          "texte": ["Oui ! Je dois dire, je suis plutôt impressionné ! Je ne pensais pas qu’il y aurait autant d’histoires pour cette école !"],
           "suivant": {"personnage": "Emma", "slide": "10", "paragraphe": "3"}
         },
         {
@@ -184,19 +184,19 @@ let Personnage = {
     "dialogues": {
       "5": [
         {
-          "texte": ["Non, pas tant que ça ! Il faut dire que j’ai eu de la chance :^500"," je suis arrivé dans des locaux flambant neufs, sur le campus des facultés de Nantes !^500"," Et l’année d’après, de nouveaux ordinateurs ont été installés !^500"],
+          "texte": ["Non, pas tant que ça ! Il faut dire que j’ai eu de la chance : je suis arrivé dans des locaux flambant neufs, sur le campus des facultés de Nantes ! Et l’année d’après, de nouveaux ordinateurs ont été installés !"],
           "suivant": {"personnage": "Pablo", "slide": "5", "paragraphe": "1"}
         },
         {
-          "texte": ["Oui, un très bon timing !^300"," Surtout que le premier mini-ordinateur est arrivé dans l’école pendant ma dernière année !^500"," Bon, à l’époque, ça n’avait de “mini” que le nom mais autant dire que j’ai accueilli ça à bras ouverts !^500"],
+          "texte": ["Oui, un très bon timing ! Surtout que le premier mini-ordinateur est arrivé dans l’école pendant ma dernière année ! Bon, à l’époque, ça n’avait de “mini” que le nom mais autant dire que j’ai accueilli ça à bras ouverts !"],
           "suivant": {"personnage": "Pablo", "slide": "5", "paragraphe": "2"}
         },
         {
-          "texte": ["Évidemment ! Je suis diplômé de la promo 1975 au sein du cursus “Finance-Comptabilité”,^500"," donc forcément, un ordinateur est un outil plus qu’important pour travailler dans les meilleures conditions.^500"],
+          "texte": ["Évidemment ! Je suis diplômé de la promo 1975 au sein du cursus “Finance-Comptabilité”, donc forcément, un ordinateur est un outil plus qu’important pour travailler dans les meilleures conditions."],
           "suivant": {"personnage": "Pablo", "slide": "5", "paragraphe": "3"}
         },
         {
-          "texte": ["Bien sûr !^300"," Cela fait 2 ans que je suis diplômé et j’ai toujours eu un poste à responsabilités depuis que je suis sorti de l’école.^500"," Aujourd’hui, je travaille au service comptabilité de KPMG à Paris. Ça te parle ?^500"],
+          "texte": ["Bien sûr ! Cela fait 2 ans que je suis diplômé et j’ai toujours eu un poste à responsabilités depuis que je suis sorti de l’école. Aujourd’hui, je travaille au service comptabilité de KPMG à Paris. Ça te parle ?"],
           "suivant": {"personnage": "Pablo", "slide": "5", "paragraphe": "4"}
         },
         {
@@ -212,15 +212,15 @@ let Personnage = {
     "dialogues": {
       "6": [
         {
-          "texte": ["Bonjour Pablo, moi c’est Martine.^300"," Et je serais ravie de t’expliquer mon parcours ainsi que l’histoire de l’école, en tout cas l’histoire que j’ai pu voir.^500"," J’ai été diplômée en 1980 du cursus “distribution - commerce  extérieur”.^400"," Ce fut une très belle expérience pour moi.^400"," J’ai eu la chance de faire partie de la première promotion diplômée de la MBA de l’oregon state university.^600"],
+          "texte": ["Bonjour Pablo, moi c’est Martine. Et je serais ravie de t’expliquer mon parcours ainsi que l’histoire de l’école, en tout cas l’histoire que j’ai pu voir. J’ai été diplômée en 1980 du cursus “distribution - commerce  extérieur”. Ce fut une très belle expérience pour moi. J’ai eu la chance de faire partie de la première promotion diplômée de la MBA de l’Oregon State University."],
           "suivant": {"personnage": "Pablo", "slide": "6", "paragraphe": "1"}
         },
         {
-          "texte": ["Bien sûr ,^300"," il s’agit d’un diplôme international d’études supérieures de haut niveau de gestion et de management des entreprises.^500"," Faire partie des diplômés fut vraiment une fierté pour moi ainsi que mes parents.^500"],
+          "texte": ["Bien sûr , il s’agit d’un diplôme international d’études supérieures de haut niveau de gestion et de management des entreprises. Faire partie des diplômés fut vraiment une fierté pour moi ainsi que mes parents."],
           "suivant": {"personnage": "Pablo", "slide": "6", "paragraphe": "2"}
         },
         {
-          "texte": ["En effet mais ce n’est pas tout, la période que j’ai vécu en tant qu’étudiante à Audencia fut très riche en innovations.^500"," Ce qui m’a particulièrement plu, c’est l’ouverture  à l’internationale.^400"," Quel chance pour nous ainsi que les futurs étudiants!^400"," Un promotion entière est parties au Polytechnic Central London.^500"," Pour nous ce fut une nouveauté et une opportunité incroyable.^500"," Puis l’ouverture d’un MBA à audencia et d’un institut de relations internationales^500"," a fait d’Audencia une école moderne.^400"," Et c’est sur cette note positive que j’ai terminé mes études.^500"],
+          "texte": ["En effet mais ce n’est pas tout, la période que j’ai vécu en tant qu’étudiante à Audencia fut très riche en innovations. Ce qui m’a particulièrement plu, c’est l’ouverture à l’internationale. Quel chance pour nous ainsi que les futurs étudiants! Un promotion entière est partie au Polytechnic Central London. Pour nous ce fut une nouveauté et une opportunité incroyable. Et puis l’ouverture d’un MBA et d’un institut de relations internationales a fait d’Audencia une école moderne. Et c’est sur cette note positive que j’ai terminé mes études."],
           "suivant": {"personnage": "Pablo", "slide": "6", "paragraphe": "3"}
         },
         {
@@ -230,4 +230,3 @@ let Personnage = {
     }
   }
 }
-
